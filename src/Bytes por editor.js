a75e32cd54977b6c70225a3f817cdc525320033b
Helder <he7d3r@gmail.com>
2014-08-18 18:11:02 -0300

    Inform the license: CC BY-SA 3.0

--- src/Bytes por editor.js
+++ src/Bytes por editor.js
@@ -1,6 +1,7 @@
 /**
  * Bytes por editor
- * @author: [[User:Helder.wiki]]
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
  * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Bytes por editor.js]] ([[File:User:Helder.wiki/Tools/Bytes por editor.js]])
  */
 /*jslint browser: true, white: true, plusplus: true, devel: true*/
