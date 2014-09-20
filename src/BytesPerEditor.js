e8a679a43f9abfb723534c63f418f4f3a4f3c08e
Helder <he7d3r@gmail.com>
2014-09-20 11:22:27 -0300

    Remove tracking link (only useful on wiki)

--- src/BytesPerEditor.js
+++ src/BytesPerEditor.js
@@ -2,7 +2,6 @@
  * Bytes por editor
  * @author: Helder (https://github.com/he7d3r)
  * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
- * @tracking: [[Special:GlobalUsage/User:Helder.wiki/Tools/Bytes por editor.js]] ([[File:User:Helder.wiki/Tools/Bytes por editor.js]])
  */
 /*jslint browser: true, white: true, plusplus: true, devel: true*/
 /*global mediaWiki, jQuery */
